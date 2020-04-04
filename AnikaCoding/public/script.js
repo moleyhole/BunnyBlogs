9bb88d037b4f74d98efbb08be38e10e0c93e1187
pmehtaaustin <pmehta@austincc.edu>
2020-04-04 01:04:50 -0500

    made a nodejs webserver

---
+++ AnikaCoding/public/script.js
@@ -0,0 +1,5 @@
+var stupid = document.getElementById("stupid");
+
+stupid.addEventListener("click", function(){
+    alert("You are wrong! Better luck next time!");
+});
--- AnikaCoding/script.js
+++
@@ -1,5 +0,0 @@
-var stupid = document.getElementById("stupid");
-
-stupid.addEventListener("click", function(){
-    alert("You are wrong! Better luck next time!");
-});
