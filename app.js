fc27de833c0b865e482c2e1af8061ac78a1bae8a
pmehtaaustin <pmehta@austincc.edu>
2020-04-04 01:27:39 -0500

    added / route

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT | 3000
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hi there')
+})
 
 app.listen(PORT, () => {
     console.log('Listening on '+ PORT)
