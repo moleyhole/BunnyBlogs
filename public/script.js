c4b836c84801048b69898e3ff909f3fd66976356
Anika <anikabmehta@gmail.com>
2020-04-05 10:40:49 -0500

    updates on sunday

--- public/script.js
+++ public/script.js
@@ -3,3 +3,29 @@ var stupid = document.getElementById("stupid");
 stupid.addEventListener("click", function(){
     alert("You are wrong! Better luck next time!");
 });
+
+var love = document.getElementById("love");
+
+love.addEventListener("click", function(){
+    alert("I love them both sooooooooooooo much eaqualy!!");
+});
+
+document.getElementById("lily").addEventListener("click", function(){
+    alert("Her name is Lily!");
+});
+
+document.getElementById("Mo").addEventListener("click", function(){
+    alert("His name is Mo!");
+});
+
+document.getElementById("Bailey").addEventListener("click", function(){
+    alert("His name is Bailey!");
+});
+
+var question = document.getElementById("question");
+
+question.addEventListener("click", function(){
+    alert("I wish I did more HTML");
+});
+
+
